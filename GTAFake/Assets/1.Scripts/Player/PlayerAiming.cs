2c31f730d8a3fc6cf7321c69ef0519840729240a
Trung nq <trung.nq24092000@gmail.com>
2024-03-31 15:34:45 +0700

    Add animation

--- GTAFake/Assets/1.Scripts/Player/PlayerAiming.cs
+++ GTAFake/Assets/1.Scripts/Player/PlayerAiming.cs
@@ -23,26 +23,28 @@ public class PlayerAiming : MonoBehaviour
         UserInputController.Instance.OnAimingJoystick -= Aim;
         UserInputController.Instance.OnCancelAiming -= HideAimingZone;
     }
-    float lasttimeShot = 0;
     private void Aim(float hzInput, float vInput)
     {
         if (Controller.IsAiming == true)
         {
-            Vector2 AimInput = new Vector2(hzInput, vInput);
-            Vector2 Direction = (new Vector2(hzInput, vInput)).normalized;
-            if (AimInput.magnitude > 0.1f)
-            {
 
-                float rotation = Mathf.Atan2(Direction.x, Direction.y) * Mathf.Rad2Deg + Controller.ForwardAxis.eulerAngles.y;
-                rotation = (rotation % 360 + 360) % 360;
-                float currentRotation = (transform.eulerAngles.y % 360 + 360) % 360;
-                if (Mathf.Abs(currentRotation - rotation) > 180)
-                {
-                    if (currentRotation < 180) currentRotation += 360;
-                    else rotation += 360;
-                }
-                transform.eulerAngles = Vector3.up * Mathf.Lerp(currentRotation, rotation, 10 * Time.deltaTime);
-            }
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, Controller.ForwardAxis.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+
+            //Vector2 AimInput = Controller.ForwardAxis.forward;
+            //Vector2 Direction = (AimInput).normalized;
+            //if (AimInput.magnitude > 0.1f)
+            //{
+
+            //    float rotation = Mathf.Atan2(Direction.x, Direction.y) * Mathf.Rad2Deg;
+            //    rotation = (rotation % 360 + 360) % 360;
+            //    float currentRotation = (transform.eulerAngles.y % 360 + 360) % 360;
+            //    if (Mathf.Abs(currentRotation - rotation) > 180)
+            //    {
+            //        if (currentRotation < 180) currentRotation += 360;
+            //        else rotation += 360;
+            //    }
+            //    transform.eulerAngles = Vector3.up * Mathf.Lerp(currentRotation, rotation, 10 * Time.deltaTime);
+            //}
 
             //shoot
         }
