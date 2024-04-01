078b2a9ca448f71a68f81f7988d6726b37849f81
Trung nq <trung.nq24092000@gmail.com>
2024-04-01 11:32:01 +0700

    Update game

--- GTAFake/Assets/1.Scripts/FPSMeasurement.cs
+++ GTAFake/Assets/1.Scripts/FPSMeasurement.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class FPSMeasurement : MonoBehaviour
 {
     float LastTimeCheck = 0;
     int i = 0;
+    private TextMeshProUGUI m_TextMeshProUGUI;
+    private void Start()
+    {
+        m_TextMeshProUGUI = GetComponent<TextMeshProUGUI>();
+    }
     void Update()
     {
         LastTimeCheck += Time.deltaTime;
         i++;
         if (LastTimeCheck >= 1)
         {
-            Debug.Log("FPS:" + i);
+            if (m_TextMeshProUGUI != null)
+            {
+                m_TextMeshProUGUI.SetText("FPS:" + i);
+            }
+            else
+                Debug.Log("FPS:" + i);
             i = 0;
             LastTimeCheck = 0;
         }
